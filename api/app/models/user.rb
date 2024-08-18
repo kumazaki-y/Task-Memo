d3ec4987777dbed1c72fd0a26bd1af33430fbebe
山崎柊 <>
2024-08-18 10:43:05 +0900

    deviseとtokenライブラリのインストール、設定、マイグレーション実行。

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+          :recoverable, :rememberable, :trackable, :validatable,
+          :confirmable, :omniauthable
+
+  # DeviseTokenAuthのモジュールをインクルード
+  include DeviseTokenAuth::Concerns::User
 end
